cc022a0beb10fbd1cc8954fe9eddd7be6e033585
thiruu <neelakantan.roshan@gmail.com>
2016-08-22 13:34:21 -0400

    Changed the file name more meaningful

--- write.R
+++ write.R
@@ -1,5 +1,5 @@
 # Read the CSV file
-threexl <- read.csv("three.csv", header = TRUE)
+csvfile <- read.csv("three.csv", header = TRUE)
 
 # Connect to my oracle database
 library(RODBC)
@@ -9,10 +9,10 @@ channel <- odbcConnect("ORCL", uid="hr", pwd = "hr", believeNRows = FALSE)
 odbcGetInfo(channel)
 
 # SQL query to create a similarly structured table as that of the CSV file
-sqlQuery(channel, "create table three_db (id int, name varchar(100), subject varchar(100), score int, year int, role varchar(100))")
+sqlQuery(channel, "create table file_db (id int, name varchar(100), subject varchar(100), score int, year int, role varchar(100))")
 
 # Insert contents of csv file into the newly created table in my oracle database
-sqlSave(channel, threexl, tablename = "three_db", rownames = FALSE, append = TRUE)
+sqlSave(channel, csvfile, tablename = "file_db", rownames = FALSE, append = TRUE)
 
 # Disconnect
 odbcClose(channel)
